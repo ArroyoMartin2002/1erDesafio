623bb274c6762b0cbbab072b126a25d6b4f9be8d
Martin Arroyo <arroyomartin2002@gmail.com>
2022-09-06 13:23:35 -0300

    Agregue una funcion al calculo de las cuota

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Aqui podra calcular el monto a pagar en relacion a la cantidad de cuotas ofrecudas con sus respectivos intereses")
+alert("Aqui podra calcular el monto a pagar en relacion a la cantidad de cuotas ofrecidas con sus respectivos intereses")
 
 let monto = parseInt(prompt("Ingrese el monto a abonar: "));
 
@@ -8,19 +8,24 @@ while(cuotas != "3" && cuotas !="6" && cuotas != "12" ) {
     cuotas = parseInt(prompt("ingrese un valor valido (3,6,12): "));
 }
 
+let calculoCuotas = (monto,interes,cuotas) => {
+    return (monto * interes) / cuotas;
+}
+
 switch(cuotas){
     case 3:
-        alert("Usted debera pagar 3 cuotas de " + ((monto * 1.13) / 3) +  " resultando en un monto total aproximado de " + monto * 1.13);;
+        alert("Usted debera pagar 3 cuotas de " + calculoCuotas(monto,1.13,cuotas) +  " resultando en un monto total aproximado de " + monto * 1.13);
         break;
     case 6:
-        alert("Usted debera pagar 6 cuotas de " + ((monto * 1.21) / 6) +  " resultando en un monto total aproximado de " + monto * 1.21);;
+        alert("Usted debera pagar 6 cuotas de " + calculoCuotas(monto,1.21,cuotas) +  " resultando en un monto total aproximado de " + monto * 1.21);
         break;
     case 12:
-        alert("Usted debera pagar 12 cuotas de " + ((monto * 1.36) / 12) +  " resultando en un monto total aproximado de " + monto * 1.36);;
+        alert("Usted debera pagar 12 cuotas de " + calculoCuotas(monto,1.36,cuotas) +  " resultando en un monto total aproximado de " + monto * 1.36);
         break;
     default:
         alert("?")
         break;
 }
+
 
 
